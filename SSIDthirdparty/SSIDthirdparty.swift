14d31f840989b30aaa6092f0094822c3ec9d26a7
Archan <archan@klizos.com>
2019-07-26 12:45:43 +0530

    Added test cases

---
+++ SSIDthirdparty/SSIDthirdparty.swift
@@ -0,0 +1,23 @@
+//
+//  SSIDthirdparty.swift
+//  SSIDthirdparty
+//
+//  Created by iOS Development on 7/26/19.
+//  Copyright © 2019 iOS Development. All rights reserved.
+//
+
+import Foundation
+
+public final class SSIDthirdparty {
+    
+    let name = "SSIDthirdparty"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- SSIDthirdpartyTests/SSIDthirdpartyTests.swift
+++ SSIDthirdpartyTests/SSIDthirdpartyTests.swift
@@ -10,10 +10,23 @@ import XCTest
 @testable import SSIDthirdparty
 
 class SSIDthirdpartyTests: XCTestCase {
-
+    
+    
+    var SSIDthirdpartyLib: SSIDthirdparty!
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        SSIDthirdpartyLib = SSIDthirdparty()
     }
+    
+    func testAdd() {
+        XCTAssertEqual(SSIDthirdpartyLib.add(a: 1, b: 1), 2)
+    }
+    
+    func testSub() {
+        XCTAssertEqual(SSIDthirdpartyLib.sub(a: 2, b: 1), 1)
+    }
+
+    
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
