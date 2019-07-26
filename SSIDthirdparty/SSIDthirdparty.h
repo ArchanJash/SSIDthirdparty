81f171bc764be8e1ab3608fc31d103a5bb3b1f02
Archan <archan@klizos.com>
2019-07-26 12:39:48 +0530

    Initial project setup

---
+++ SSIDthirdparty/SSIDthirdparty.h
@@ -0,0 +1,19 @@
+//
+//  SSIDthirdparty.h
+//  SSIDthirdparty
+//
+//  Created by iOS Development on 7/26/19.
+//  Copyright © 2019 iOS Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SSIDthirdparty.
+FOUNDATION_EXPORT double SSIDthirdpartyVersionNumber;
+
+//! Project version string for SSIDthirdparty.
+FOUNDATION_EXPORT const unsigned char SSIDthirdpartyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSIDthirdparty/PublicHeader.h>
+
+
